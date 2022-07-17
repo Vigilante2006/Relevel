fc236aa254ab6f4dfbfcee1457ce0e5b075271cc
Vigilante2006 <82402212+Vigilante2006@users.noreply.github.com>
2022-07-17 19:14:52 +0530

    second

--- OTH.js
+++
@@ -1,9 +0,0 @@
-for (let i = 1; i <= 100; i++) {
-  i % 15 == 0
-    ? console.log(i," => MindBuzz")
-    : i % 3 == 0
-    ? console.log(i," => Relevel")
-    : i % 5 == 0
-    ? console.log(i," => Buzz")
-    : console.log(i," => tSe number");
-}
